23f9f75569ea59cf90df2601434cce33fc0ded98
Neil Chen <neilwchen@gmail.com>
2014-08-27 16:57:22 -0400

    forget grid layout, just find mouse position

--- austin/main.py
+++ austin/main.py
@@ -1,30 +1,7 @@
 from Tkinter import *
 
-
 gui = Tk()
 
-class App:
-	def __init__(self, parent):
-		self.tk = parent
+gui.minsize(width=1000, height=1000)
 
-	def callback(self):
-		print 'click'
-
-	def build_grid(self, grid_size, cell_size):
-		grid = []
-		for row in range(0, grid_size):
-			grid.append([])
-			for column in range(0, grid_size):
-				grid[row].append(Button(self.tk, bg="#dadfe1", activebackground="#2d2d2d", relief=FLAT, height=cell_size/2, width=cell_size, command=self.callback))
-				grid[row][-1].bind('<Button-1>', self.click(grid[row][-1]))
-				grid[row][-1].grid(row=row, column=column, padx=1, pady=1)
-
-	def click(self, event):
-		if self.button['bg']=="#dadfe1":
-			button.configure(bg="#000000")
-		else:
-			button.configure(bg="#dadfe1")
-
-app = App(gui)
-app.build_grid(20, 6)
 mainloop()
