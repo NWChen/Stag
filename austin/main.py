488a31ec954ec93cdca958b7568bcf5481a52a44
Neil Chen <neilwchen@gmail.com>
2014-08-27 17:35:51 -0400

    simple no-grid click

--- austin/main.py
+++ austin/main.py
@@ -1,7 +1,27 @@
 from Tkinter import *
 
-gui = Tk()
+class App:
 
-gui.minsize(width=1000, height=1000)
+	def __init__(self, tk):
+		self.tk = tk
+		self.tk.bind('<Motion>', self.motion)
+		self.tk.bind('<Button-1>', self.click)
+		self.canvas = Canvas(self.tk, width=1000, height=1000, cursor="crosshair", bg="white")
+		self.canvas.pack()
+		self.x = 0
+		self.y = 0
 
-mainloop()
+	def motion(self, event):
+		self.x = event.x
+		self.y = event.y
+		print self.x, self.y
+
+	def click(self, event):
+		self.canvas.create_rectangle()
+
+	def mainloop(self):
+		tk.mainloop()
+
+tk = Tk()
+app = App(tk)
+app.mainloop()
