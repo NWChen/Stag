7c2b29a0f48e6dc49748aca638b7980ff38d7243
Neil Chen <neilwchen@gmail.com>
2014-08-28 15:01:56 -0400

    tkinter mouse over

--- austin/ostin.py
+++ austin/ostin.py
@@ -14,8 +14,14 @@ class App:
 		self.size = 1000
 		self.canvas = Canvas(self.tk, width=self.size, height=self.size, cursor="crosshair", bg="white")
 		self.canvas.grid(row=0, column=0)
-		
-		self.
+		self.inCanvas = True
+
+		self.listbox = Listbox(tk, selectmode=SINGLE)
+		self.listbox.grid(row=0, column=1)
+		self.scrollbar = Scrollbar(tk)
+		self.scrollbar.grid(row=0, column=2)
+		self.listbox.config(yscrollcommand=self.scrollbar.set)
+		self.scrollbar.config(command=self.listbox.yview)
 
 		self.x = 0
 		self.y = 0
@@ -29,14 +35,19 @@ class App:
 		print(self.x, self.y)
 
 	#callback when left mouse button clicked
-	def click(self, event):
-		self.add_point(self.x, self.y)
+	def click(self, event): 
+		self.add_point(Point(self.x, self.y, self.canvas))
 
 	#add a Point to the canvas and points list
-	def add_point(self, x, y):
-		point = Point(self.x, self.y, self.canvas)
+	def add_point(self, point):
 		self.points.append(point)
+		self.listbox.insert(END, str(point))
 		self.num_waypoints += 1
+
+	#delete a Point
+	def delete_point(self, point):
+		self.points.remove(point)
+		#self.listbox.delete()
 
 	#main function
 	def mainloop(self):
