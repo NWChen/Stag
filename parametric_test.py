eebe2c357f25ce8c4c91ad3dc2cea8f8ee373504
Neil Chen <neilwchen@gmail.com>
2014-08-29 17:23:17 -0400

    add parametric test

--- parametric_test.py
+++ parametric_test.py
@@ -3,13 +3,15 @@ import math
 
 gui = Tk()
 
-canvas = Canvas(gui, width=100, height=100, bg="white")
+canvas = Canvas(gui, width=400, height=400, bg="white")
 canvas.pack()
+x, y = 0, 0
 
-for t in range(0, 7):
-	x = 100*math.sin(t)
-	y = 100*math.cos(t)
+for t in range(0, 30):
+	t /= float(5)
+	x = 100*math.sin(t)+200
+	y = 100*math.cos(t)+200
 	print x, y
-	canvas.create_oval(x-2, y-2, x+2, y+2, outline='red')
+	canvas.create_oval(x-2, y-2, x+2, y+2, fill='black', outline='black')
 
 gui.mainloop()
