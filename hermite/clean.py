057fd684e72921fd5c15591b8679e033021764af
NWChen <neilwchen@gmail.com>
2014-11-11 15:02:22 -0500

    moving tests over

--- hermite/clean.py
+++ hermite/clean.py
@@ -51,12 +51,13 @@ ta, tb, tc = [30, 200], [40, 1000], [90, 500]
 build_point(a, 'red', 3)
 build_point(b, 'red', 3)
 build_point(c, 'red', 3)
-
-cubic_interpolate(a, b, convert_to_derivative(ta[0], ta[1]), convert_to_derivative(tb[0], tb[1]), 500)
-cubic_interpolate(b, c, convert_to_derivative(tb[0], tb[1]), convert_to_derivative(tc[0], tc[0]), 500)
 build_tangent(a, convert_to_derivative(ta[0], ta[1]), 'red')
 build_tangent(b, convert_to_derivative(tb[0], tb[1]), 'red')
 build_tangent(c, convert_to_derivative(tc[0], tc[1]), 'red')
+
+cubic_interpolate(a, b, convert_to_derivative(ta[0], ta[1]), convert_to_derivative(tb[0], tb[1]), 100)
+cubic_interpolate(b, c, convert_to_derivative(tb[0], tb[1]), convert_to_derivative(tc[0], tc[0]), 100)
+
 
 '''
 x, up = 0, True
