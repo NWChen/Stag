9fa30863bcedc8bf2972e2317fe9ea4b69a4d9bf
NWChen <neilwchen@gmail.com>
2014-11-11 13:46:09 -0500

    derivative no longer constant in hermite

---
+++ hermite/Derivative.py
@@ -0,0 +1,7 @@
+from Point import Point
+
+class Derivative(Point):
+
+	def __init__(self, dy, dx):
+		self.dy = dy
+		self.dx = dx
--- hermite/clean.py
+++ hermite/clean.py
@@ -1,5 +1,6 @@
 import math
 import time
+from Derivative import Derivative
 from Point import Point
 from tkinter import *
 
@@ -9,24 +10,25 @@ def build_point(p, color='black', size=1):
 def cubic_interpolate(p1, p2, t1, t2, steps):
 	for t in range(0, steps):
 		s = t/float(steps)
-		h1 = (2*math.pow(s,3.0)) - (3*math.pow(s,2)) + 1
-		h2 = (-2*math.pow(s,3)) + (3*math.pow(s,2))
-		h3 = (math.pow(s,3)) - (2*math.pow(s,2)) + s
-		h4 = (math.pow(s,3)) - (math.pow(s,2))
-		p = Point(h1*p1.x + h2*p2.x + h3*t1 + h4*t2,
-			h1*p1.y + h2*p2.y + h3*t1 + h4*t2)
+		h1 = 2*s*s*s - 3*s*s + 1
+		h2 = -2*s*s*s + 3*s*s
+		h3 = s*s*s - 2*s*s + s
+		h4 = s*s*s - s*s
+		p = Point(h1*p1.x + h2*p2.x + h3*t1.dx + h4*t2.dx,
+			h1*p1.y + h2*p2.y + h3*t1.dy + h4*t2.dy)
 		build_point(p)
 	return
 
-def quintic_interpolate(p1, p2, t1, t2, steps):
+def quintic_interpolate(p1, p2, d1, d2, dd1, dd2, steps):
 	for t in range(0, steps):
 		s = t/float(steps)
 		h0 = 1-(10*math.pow(s,2))+(15*math.pow(t,4))-(6*math.pow(t,5))
-		h1 = s-(6*math.pow(s,3)+(8*math.pow(s,4))-(3*math.pow(s,5))
+		h1 = s-(6*math.pow(s,3))+(8*math.pow(s,4))-(3*math.pow(s,5))
 		h2 = (0.5*math.pow(s,2))-(1.5*math.pow(s,3))+(1.5*math.pow(s,4))-(0.5*math.pow(t,5))
 		h3 = (0.5*math.pow(s,3))-(math.pow(s,4))+(0.5*math.pow(s,5))
 		h4 = (-4*math.pow(s,3))+(7*math.pow(s,4))-(3*math.pow(s,5))
 		h5 = (10*math.pow(s,3))-(15*math.pow(s,4))+(6*math.pow(s,5))
+		p = Point(h0*p1.x + h1*d1)
 
 def build_tangent(p, slope, color='black'):
 	x = p.x
@@ -45,13 +47,13 @@ while(True):
 		x += 10
 	else:
 		x -= 10
-	if x>500:
+	if x>200:
 		up = False
-	elif x<-500:
+	elif x<(-200):
 		up = True
-	cubic_interpolate(Point(300, 400), Point(400, 400), x, 0, 100)
+	cubic_interpolate(Point(300, 400), Point(600, 400), Derivative(-1000, x), Derivative(0, 1000), 100)
 	build_tangent(Point(300, 400), x)
-	canvas.after(60)
+	canvas.after(15)
 	canvas.update()
 	canvas.delete("all")
 
